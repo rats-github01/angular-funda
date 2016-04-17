80104edf5afb6ffb7c422b40075b542d31d064ce
rats-github01 <sarathbm4it@gmail.com>
2016-04-16 17:22:43 -0700

    Added Karma test config and Test scripts

---
+++ M1/tests/specs.js
@@ -0,0 +1,23 @@
+/**
+ * Created by hari on 4/16/2016.
+ */
+
+describe('Hello World', function() {
+
+    it('should increment a value by one', function() {
+        expect(add(1)).toBe(2);
+
+        function add(value) {
+            return value + 1;
+        };
+    });
+
+    it('should decrement a value by one', function() {
+        expect(subtract(1)).toBe(0);
+
+        function subtract(value) {
+            return value - 1;
+        };
+    });
+
+});
