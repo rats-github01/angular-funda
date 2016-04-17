2f71f1decb5e1d575b5fe3c525065273fde3d944
rats-github01 <sarathbm4it@gmail.com>
2016-04-17 16:18:09 -0700

    Added Controllers with sample routing and template referenceing. This works as a simple website template.

--- M3/app.js
+++ M3/app.js
@@ -1,8 +1,25 @@
-angular.module('MyApp', [])
+angular.module('MyApp', ['ngRoute'])
 
     .constant('VERSION', '1.1')
+
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/', {
+            controller: 'HomeCtrl',
+            templateUrl: './home.html'
+        })
+        .when('/info', {
+            controller: 'InfoCtrl',
+            templateUrl: './info.html'
+        });
+    }])
 
     .controller('HomeCtrl', ['$scope', function($scope) {
     //some controller code for the HomeCtrl
         $scope.title = 'Welcome to the HomePage';
-}]);
+        $scope.description = function() {
+            return 'this is sample description';
+        };
+    }])
+    .controller('InfoCtrl', ['$scope', function($scope) {
+        $scope.title = 'Welcome to Info Page!';
+    }]);
