3e2a46f4db04c4184bdce6129588383d0342d0fd
rats-github01 <sarathbm4it@gmail.com>
2016-04-17 21:29:46 -0700

    Added ng-class sample and Form with validations

--- M4/app.js
+++ M4/app.js
@@ -2,6 +2,23 @@ angular.module('MyApp', [])
     .controller('Other2Ctrl', ['$scope', function($scope) {
         $scope.title = 'Other Slide 2...';
     }])
+    .controller('FormCtrl', ['$scope', function($scope) {
+        $scope.generateAges = function() {
+            var ages = [];
+            for(var i=18; i <= 60; i++){
+                ages.push(i);
+            }
+            return ages;
+        };
+        $scope.submit = function() {
+            //alert('Submitted');
+          if($scope.myForm.$valid){
+              alert('submitted');
+          }  else {
+              alert('invalid');
+          }
+        };
+    }])
     .controller('RepeatCtrl', ['$scope', function($scope) {
         $scope.setColors = function() {
             $scope.values = ['Yellow', 'Blue', 'Green', 'Red', 'White'];
