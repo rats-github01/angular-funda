9a2cfcb53e904d75581be9db8a6a6bf0df10e016
rats-github01 <sarathbm4it@gmail.com>
2016-04-18 00:37:17 -0700

    Added  sample with local file as resource

--- M5/app.js
+++ M5/app.js
@@ -1,4 +1,14 @@
 angular.module('MyApp', [])
+
+    .controller('ListCtrl', ['$scope', '$http', function($scope, $http) {
+        $http.get('./users.json').success(function(users) {
+           $scope.users = users;
+        });
+    }]);
+
+
+/*
+angular.module('MyApp', [])
     .factory('userService', function() {
         var user;
         return {
@@ -16,4 +26,4 @@ angular.module('MyApp', [])
     }])
     .controller('InfoCtrl', ['$scope', 'userService', function($scope, userService) {
         userService.setCurrentUser('us001');
-    }])
+    }])*/
